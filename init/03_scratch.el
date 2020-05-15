96eb56ae9dbde46edb4b503d157357bd00f1983c
cahlchang <kahlua.dane@gmail.com>
2020-05-15 23:21:29 +0900

    add vue mode

---
+++ init/03_scratch.el
@@ -0,0 +1,13 @@
+(use-package auto-save-buffers-enhanced)
+;; Suppress Wrote's message
+(setq auto-save-buffers-enhanced-quiet-save-p t)
+;; Scratch buffer is also saved automatically
+(setq auto-save-buffers-enhanced-save-scratch-buffer-to-file-p t)
+(setq auto-save-buffers-enhanced-file-related-with-scratch-buffer "~/.emacs.d/scratch")
+(auto-save-buffers-enhanced t)
+
+(defun my:pop-scratch ()
+  "Popup the scratch buffer."
+  (interactive)
+  (setq popwin:special-display-config '(("*scratch*")))
+  (display-buffer "*scratch*"))
---
+++ init/61-js.el
@@ -0,0 +1,44 @@
+
+(use-package js-mode
+  :mode ("\\.json$" . js-mode)
+  :init
+  (progn
+    (add-hook 'js-mode-hook
+	      (lambda ()
+		(make-local-variable 'js-indent-level)
+		(setq js-indent-level 2)))))
+
+(setq js-indent-level 2)
+
+;; ************************************************************************
+;; mmm-mode
+;; ************************************************************************
+(use-package mmm-mode
+  :ensure t
+  :config
+  (setq mmm-global-mode 'maybe)
+  (setq mmm-submode-decoration-level 2)
+  (set-face-background 'mmm-default-submode-face "gray13")
+
+  (mmm-add-classes
+   '((vue-embeded-slim-mode
+      :submode slim-mode
+      :front "^<template.*lang=\"pug\">\n"
+      :back "^</template>")
+     (vue-embeded-web-mode
+      :submode web-mode
+      :front "^<template>\n"
+      :back "^</template>\n")
+     (vue-embeded-js-mode
+      :submode js-mode
+      :front "^<script>\n"
+      :back "^</script>")
+     (vue-embeded-scss-mode
+      :submode scss-mode
+      :front "^<style.*lang=\"scss\">\n"
+      :back "^</style>")))
+
+  (mmm-add-mode-ext-class nil "\\.vue\\'" 'vue-embeded-slim-mode)
+  (mmm-add-mode-ext-class nil "\\.vue\\'" 'vue-embeded-web-mode)
+  (mmm-add-mode-ext-class nil "\\.vue\\'" 'vue-embeded-js-mode)
+  (mmm-add-mode-ext-class nil "\\.vue\\'" 'vue-embeded-scss-mode))
